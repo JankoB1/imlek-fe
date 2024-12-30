99885e61ca168b12c240e8e9090c4ba4bb13cea7
Janko Blagojevic <janko.tbbt@gmail.com>
2024-12-30 22:49:33 +0100

    post request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FileUploaderComponent } from './file-uploader/file-uploader.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     FileUploaderComponent
   ],
   imports: [
-
+    HttpClientModule
   ],
   providers: [],
   exports: [
--- src/app/file-uploader/file-uploader.component.ts
+++ src/app/file-uploader/file-uploader.component.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-file-uploader',
   templateUrl: './file-uploader.component.html',
-  styleUrls: ['./file-uploader.component.css']
+  styleUrls: ['./file-uploader.component.css'],
 })
 export class FileUploaderComponent {
-  uploadedFiles: File[] = [];
+  uploadedFiles: { file: File; base64: string }[] = [];
+
+  constructor(private http: HttpClient) {}
 
   onFileSelected(event: Event, buttonId: number): void {
     const input = event.target as HTMLInputElement;
 
     if (input.files && input.files.length > 0) {
       const selectedFile = input.files[0];
-      this.uploadedFiles.push(selectedFile);
-
       this.convertFileToBase64(selectedFile).then((base64: string) => {
         console.log(`Base64 representation of file uploaded via Button ${buttonId}:`, base64);
 
+        // Store the file and its Base64 representation
+        this.uploadedFiles[buttonId - 1] = { file: selectedFile, base64 };
       });
     }
   }
@@ -38,4 +40,29 @@ export class FileUploaderComponent {
     });
   }
 
+  onCompareFiles(): void {
+    if (this.uploadedFiles.length < 2 || !this.uploadedFiles[0] || !this.uploadedFiles[1]) {
+      alert('Please upload both files before comparing.');
+      return;
+    }
+
+    const requestBody = {
+      pdf_base64: this.uploadedFiles[0]?.base64 || '',
+      xlsx_base64: this.uploadedFiles[1]?.base64 || '',
+    };
+
+    // Send POST request
+    this.http
+      .post('http://localhost:8000/compare-documents-base64', requestBody)
+      .subscribe({
+        next: (response) => {
+          console.log('Comparison result:', response);
+          alert('Comparison successful!');
+        },
+        error: (err) => {
+          console.error('Error comparing files:', err);
+          alert('Comparison failed. Please try again.');
+        },
+      });
+  }
 }
